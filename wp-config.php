323a63c9c681eb507e121212225aa1df559ab0b1
Ali Mahdavi <alihmahdavi@gmail.com>
2024-02-21 13:27:00 +0300

    defaults for older versions

--- wp-config.php
+++ wp-config.php
@@ -44,13 +44,13 @@ if ( !function_exists('getenv_docker') ) {
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv_docker('WORDPRESS_DB_NAME', '') );
+define( 'DB_NAME', getenv_docker('WORDPRESS_DB_NAME', 'test_db') );
 
 /** Database username */
-define( 'DB_USER', getenv_docker('WORDPRESS_DB_USER', '') );
+define( 'DB_USER', getenv_docker('WORDPRESS_DB_USER', 'root') );
 
 /** Database password */
-define( 'DB_PASSWORD', getenv_docker('WORDPRESS_DB_PASSWORD', '') );
+define( 'DB_PASSWORD', getenv_docker('WORDPRESS_DB_PASSWORD', '123secret') );
 
 /**
  * Docker image fallback values above are sourced from the official WordPress installation wizard:
