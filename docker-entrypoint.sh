044a5f80058879c24555d8f3cf1fe2757fb61035
Ali Mahdavi <alihmahdavi@gmail.com>
2024-02-22 11:07:54 +0300

    add check for index.php

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -57,7 +57,7 @@ for contentPath in \
     sourceTarArgs+=( --exclude "./$contentPath" )
   fi
 done
-if [ -d "/var/www/html/wp-admin" ] && [ -f "/var/www/html/wp-config.php" ]; then
+if [ -d "/var/www/html/wp-admin" ] && [ -f "/var/www/html/wp-config.php" ] && [ -f "/var/www/html/index.php" ]; then
     echo "Wordpress already there skipping..."
 else
     tar "${sourceTarArgs[@]}" . | tar "${targetTarArgs[@]}"
