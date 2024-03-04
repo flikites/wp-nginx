4f49158c22a266c8c54a3851d36335f7e985c282
Ali Mahdavi <alihmahdavi@gmail.com>
2024-03-04 10:47:31 +0300

    minor change

--- php-fpm.sh
+++ php-fpm.sh
@@ -2,25 +2,24 @@
 pidfile="/usr/local/bin/php-fpm-pid"
 # Start the process
 php-fpm -g $pidfile &
-# Wait for 10 seconds
-sleep 10
+# Wait for 5 seconds
+sleep 5
 # Kill the process
 pid=$(cat $pidfile)
 kill $pid
+sleep 5
 # loop that resests php-fpm and folder permissions each 30 minutes
 while true; do
 
     # Start the process
     php-fpm -g $pidfile &
-    # Wait for 30 minutes
-
     # reset wp folder permissions
     chmod -R g+rwx /var/www/html/
     chown -R www-data:www-data /var/www/html/
     echo "Reseted Permissions."
+    # Wait for 30 minutes
     sleep 1800
     # Kill the process
     pid=$(cat $pidfile)
-    #kill $pid
-    
+    kill $pid
 done
