e5bde0b71e07cf5ce73923b634066acbed37149c
Ali Mahdavi <alihmahdavi@gmail.com>
2024-03-04 10:05:09 +0300

    test permission issue

--- php-fpm.sh
+++ php-fpm.sh
@@ -9,14 +9,16 @@ pid=$(cat $pidfile)
 kill $pid
 # loop that resests php-fpm and folder permissions each 30 minutes
 while true; do
-    # Start the process
-    php-fpm -g $pidfile &
-    # Wait for 30 minutes
-    sleep 1800
-    # Kill the process
-    pid=$(cat $pidfile)
-    kill $pid
     # reset wp folder permissions
     chmod -R g+rwx /var/www/html/
     chown -R www-data:www-data /var/www/html/
+
+    # Start the process
+    #php-fpm -g $pidfile &
+    # Wait for 30 minutes
+    sleep 1800
+    # Kill the process
+    #pid=$(cat $pidfile)
+    #kill $pid
+    
 done
